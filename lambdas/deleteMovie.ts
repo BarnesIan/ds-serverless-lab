c808d47d16506151223e69e430c7e227699f3903
barnesian <74927667+IanBarnes96@users.noreply.github.com>
2023-10-25 11:51:24 +0100

    Exercise: Delete movie endpoint

---
+++ lambdas/deleteMovie.ts
@@ -0,0 +1,81 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, DeleteCommand, GetCommand } from "@aws-sdk/lib-dynamodb";
+
+const ddbDocClient = createDDbDocClient();
+
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
+  try {
+    // Print Event
+    console.log("Event: ", event);
+      const parameters = event?.pathParameters;
+      const movieId = parameters?.movieId ? parseInt(parameters.movieId) : undefined;
+
+
+    if (!movieId) {
+      return {
+        statusCode: 404,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Message: "Missing movie Id" }),
+      };
+    }
+
+    const getCommandOutput = await ddbDocClient.send(
+      new GetCommand({
+        TableName: process.env.TABLE_NAME,
+        Key: { movieId: movieId },
+      })
+    );
+    console.log("GetCommand response: ", getCommandOutput);
+    if (!getCommandOutput.Item) {
+      return {
+        statusCode: 404,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Message: "Invalid movie Id" }),
+      };
+    }
+
+const commandOutput = await ddbDocClient.send(
+  new DeleteCommand( {
+    TableName : process.env.TABLE_NAME,
+    Key : {movieId : movieId },
+  })
+);
+    return {
+        statusCode: 200,
+        headers: {
+          "content-type" : "application/json"
+        
+        },
+        body : JSON.stringify({message : "Movie successfully deleted"})
+    };
+
+} catch (error: any) {
+  console.log(JSON.stringify(error));
+  return {
+    statusCode: 500,
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ error }),
+  };
+}
+};
+
+function createDDbDocClient() {
+    const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+    const marshallOptions = {
+      convertEmptyValues: true,
+      removeUndefinedValues: true,
+      convertClassInstanceToMap: true,
+    };
+    const unmarshallOptions = {
+      wrapNumbers: false,
+    };
+    const translateConfig = { marshallOptions, unmarshallOptions };
+    return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+  }
